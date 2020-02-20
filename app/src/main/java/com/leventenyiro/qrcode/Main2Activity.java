99ec1401c4404aee571be1cfaca5756c79d53491
Petrik Lajos Szg <info@petrik.hu>
2020-02-20 16:20:48 +0100

    hivatkozások megnyitása

--- app/src/main/java/com/leventenyiro/qrcode/Main2Activity.java
+++ app/src/main/java/com/leventenyiro/qrcode/Main2Activity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -47,6 +48,12 @@ public class Main2Activity extends AppCompatActivity {
                 Toast.makeText(this, "Kiléptél a scannelésből!", Toast.LENGTH_SHORT).show();
             } else {
                 textEredmeny.setText("QR Code eredmény: " + result.getContents());
+
+                // ha van benne link akkor menjen rá
+
+                Uri uri = Uri.parse(result.getContents());
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
             }
         }
         super.onActivityResult(requestCode, resultCode, data);
