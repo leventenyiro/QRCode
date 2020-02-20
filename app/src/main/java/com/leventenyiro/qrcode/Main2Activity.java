58b1d1acc7bdb5ec6fe18182a135fae07d1830b9
Petrik Lajos Szg <info@petrik.hu>
2020-02-20 16:12:01 +0100

    scannelés hozzáadása

--- app/src/main/java/com/leventenyiro/qrcode/Main2Activity.java
+++ app/src/main/java/com/leventenyiro/qrcode/Main2Activity.java
@@ -1,14 +1,59 @@
 package com.leventenyiro.qrcode;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class Main2Activity extends AppCompatActivity {
+
+    private Button btnScan;
+    private TextView textEredmeny;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        init();
+
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(Main2Activity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+                integrator.setPrompt("QR Code Scaning by app");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(false);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan(); // aktiválás
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Toast.makeText(this, "Kiléptél a scannelésből!", Toast.LENGTH_SHORT).show();
+            } else {
+                textEredmeny.setText("QR Code eredmény: " + result.getContents());
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void init() {
+        btnScan = findViewById(R.id.btnScan);
+        textEredmeny = findViewById(R.id.textEredmeny);
     }
 }
