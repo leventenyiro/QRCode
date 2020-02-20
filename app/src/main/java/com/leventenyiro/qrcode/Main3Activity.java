d7617c7d0944daa82117bd33ba8a68bc0ea5522b
Petrik Lajos Szg <info@petrik.hu>
2020-02-20 16:33:53 +0100

    KÉSZ

--- app/src/main/java/com/leventenyiro/qrcode/Main3Activity.java
+++ app/src/main/java/com/leventenyiro/qrcode/Main3Activity.java
@@ -2,13 +2,56 @@ package com.leventenyiro.qrcode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class Main3Activity extends AppCompatActivity {
+
+    private EditText inputBemenet;
+    private Button btnGenerate;
+    private ImageView imageQRCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        init();
+
+        btnGenerate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (inputBemenet.getText().toString().isEmpty())
+                    Toast.makeText(Main3Activity.this, "Nincs megadva érték!", Toast.LENGTH_SHORT).show();
+                else {
+                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                    try {
+                        BitMatrix bitMatrix = multiFormatWriter.encode(inputBemenet.getText().toString(), BarcodeFormat.QR_CODE, 500, 500);
+                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                        Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                        imageQRCode.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void init() {
+        inputBemenet = findViewById(R.id.inputBemenet);
+        btnGenerate = findViewById(R.id.btnGenerate);
+        imageQRCode = findViewById(R.id.imageQRCode);
     }
 }
